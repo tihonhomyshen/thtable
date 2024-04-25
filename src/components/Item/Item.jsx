e194b751e4bb71d06f1feb92fb2b2c455dd3564c
tihonhomyshen <lilo.frich@gmail.com>
2024-04-25 17:03:32 +0300

    clear item component

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 
-const Item = ({name, sales, price1, price2, income, count1, count2}) => {
+const Item = () => {
     return (
         <>
-            <div>{name}</div>
-            <div>{sales}</div>
-            <div>{price1}</div>
-            <div>{price2}</div>
-            <div>{income}</div>
-            <div>{count1}</div>
-            <div>{count2}</div>
+
         </>
     )
 }
