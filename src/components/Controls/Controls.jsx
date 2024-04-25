12a5c08b1fd29c561575b122886a32ac260b2411
tihonhomyshen <lilo.frich@gmail.com>
2024-04-25 17:28:45 +0300

    feat: start table with <table> tag

---
+++ src/components/Controls/Controls.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useTranslation } from "react-i18next"
+import "./TableHeader.css"
+
+export const Controls = () => {
+    const { t, i18n } = useTranslation()
+    return (
+        <>
+            <div className="upper-row">
+                <p>{t("table.lastUpdate")}: 2m ago</p>
+                <div className="controls">
+                    <input className='search' type="text" placeholder={t("table.search")} />
+                    <button className="button">{t("table.update")}</button>
+                </div>
+            </div>
+        </>
+    )
+
+}
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,11 +1,24 @@
 import React from "react";
-import { TableHeader } from "../TableHeader/TableHeader";
+import { useTranslation } from "react-i18next"
 
 export const Table = () => {
+    const { t, i18n } = useTranslation()
     return (
         <>
             <div className="container">
-                <TableHeader />
+                <table>
+                    <thead>
+                        <tr>
+                            <td>{t("table.name")}</td>
+                            <td>{t("table.sales1")}</td>
+                            <td>{t("table.price1")}</td>
+                            <td>{t("table.price2")}</td>
+                            <td>{t("table.income")}</td>
+                            <td>{t("table.count1")}</td>
+                            <td>{t("table.count2")}</td>
+                        </tr>
+                    </thead>
+                </table>
             </div>
         </>
     )
